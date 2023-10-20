0d967ad9dc8ad80bbb6405ae599887f416709ad5
Md Afiq Iskandar <mdafiqiskandar@gmail.com>
2023-10-20 10:33:41 +0800

    Ensure the api can be access between fe and be

--- client/app.vue
+++ client/app.vue
@@ -6,16 +6,6 @@ const apiURL = config.public.apiBaseUrl;
 
 const name = ref("Adi");
 
-const { data } = await useFetch(apiURL + `/hello?name=${name.value}`, {
-  method: "GET",
-});
-
-console.log(data.value);
-
-if (data.value.statusCode == 200) {
-  hello.value = data.value.message;
-}
-
 const inputName = async () => {
   // Get api from localhost 3000
   const { data } = await useFetch(apiURL + `/hello?name=${name.value}`, {
